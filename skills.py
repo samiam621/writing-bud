d10d3d4a7a296387cd467153c2bbb8482a561607
samiam621 <an.samantha2007@gmail.com>
2026-07-15 17:26:02 -0700

    Initial commit

---
+++ skills.py
@@ -0,0 +1,3 @@
+#where da skills/tools go for the agent
+#where agent writes its skills but not in the doc, in its extension
+#before the user approves of the changes
---
+++ tools.py
@@ -0,0 +1,2 @@
+#tools for the agent to interact with google doc if user approves of writing changes
+# for our first goal
